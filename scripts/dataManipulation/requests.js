10760157a741bab8fc1233110a5021a667bddc87
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2022-11-07 19:57:00 -0300

    created requests

---
+++ scripts/dataManipulation/requests.js
@@ -0,0 +1,287 @@
+const baseUrl = "https://m2-api-adot-pet.herokuapp.com"
+
+async function loginRequest ( body){
+    try {
+        const request = await fetch (`${baseUrl}/session/login`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function createUser (body){
+    try {
+        const request = await fetch (`${baseUrl}/users`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function getAllUsers (token){
+    try {
+        const request = await fetch (`${baseUrl}/users`,{
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function getAllMyProfile (token){
+    try {
+        const request = await fetch (`${baseUrl}/users/profile`,{
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function updateProfile (token, body){
+    try {
+        const request = await fetch (`${baseUrl}/users/profile`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${token}`
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function deleteProfile (token){
+    try {
+        const request = await fetch (`${baseUrl}/users/profile`,{
+            method: "DELETE",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function createPet (token, body){
+    try {
+        const request = await fetch (`${baseUrl}/pets`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${token}`
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function getAllPets (token){
+    try {
+        const request = await fetch (`${baseUrl}/pets`,{
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function getAllMyPets (token){
+    try {
+        const request = await fetch (`${baseUrl}/pets/my_pets`,{
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function updatePet (token, id, body){
+    try {
+        const request = await fetch (`${baseUrl}/pets/${id}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${token}`
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function deletePetById (token, id){
+    try {
+        const request = await fetch (`${baseUrl}/pets/${id}`,{
+            method: "DELETE",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function createAdoption (token, body){
+    try {
+        const request = await fetch (`${baseUrl}/adoptions`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${token}`
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function getAllAdoptions (token){
+    try {
+        const request = await fetch (`${baseUrl}/adoptions`,{
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function getAdoptionById (token,id){
+    try {
+        const request = await fetch (`${baseUrl}/adoptions/${id}`,{
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function getMyAdoptions (token){
+    try {
+        const request = await fetch (`${baseUrl}/adoptions/myAdoptions`,{
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function updateAdoption (token, id, body){
+    try {
+        const request = await fetch (`${baseUrl}/adoptions/updtae/${id}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${token}`
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+async function deleteAdoption (token, id){
+    try {
+        const request = await fetch (`${baseUrl}/adoptions/delete/${id}`,{
+            method: "DELETE",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+        const response = await request.json()
+        console.log(response)
+
+    } catch (error) {
+        console.loge(error)
+    }
+}
+
+
