748fbcc4a9e7aee0aa2cb0f118292185c7b09778
Andreus-coder <55553397+Andreus-coder@users.noreply.github.com>
2022-11-09 09:56:33 -0300

    lateral-header upload

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,41 @@ import { createModalLogin } from "./modalLogin.js";
 import { changeToDark, checkTheme } from "./darkmode.js";
 import { closeModal } from "./closeModal.js";
 import { verticalCard } from "./cards.js";
+import { createRegisterModal } from "./modalRegister.js";
 
 /* Declarations */
 const darkButton = document.querySelector(".dark-mode")
-const button = document.querySelector("#button-login")
+const buttonLogin = document.querySelector("#button-login")
+const buttonRegister = document.getElementById("button-register")
+const openHeader = document.getElementById("open-header-btn")
+const closeHeader = document.getElementById("close-header")
 
 /* Event Listeners */
 darkButton.addEventListener("click", () => {
     changeToDark()
 })
 
-button.addEventListener("click", () => {
+buttonLogin.addEventListener("click", () => {
     const modalWrapper = document.querySelector('.modal-wrapper')
     modalWrapper.classList.toggle('show-modal')
     createModalLogin()
 })
 
+openHeader.addEventListener("click", () => {
+    const header = document.querySelector('.header')
+    header.classList.toggle('header-close')
+})
+
+closeHeader.addEventListener("click", () => {
+    const header = document.querySelector('.header')
+    header.classList.toggle('header-close')
+})
+
+buttonRegister.addEventListener("click", () => {
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    modalWrapper.classList.toggle('show-modal')
+    createRegisterModal()
+})
 /* Call to Action */
 closeModal()
 validateLocal()
